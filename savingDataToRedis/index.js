7db06bb7178eddd28ea98277901e6ef7a63f0f64
dusan rostar <rostacik@gmail.com>
2018-12-05 21:25:40 +0100

    initial commit with scraper saving data

---
+++ savingDataToRedis/index.js
@@ -0,0 +1,24 @@
+var redis = require("redis");
+var scb = require('beervana-scraper');
+
+module.exports = async function (context, myTimer) {
+    // Add your cache name and access key.
+    var client = redis.createClient(6380, process.env.REDISCACHEHOSTNAME, {
+        auth_pass: process.env.REDISCACHEKEY,
+        tls: {
+            servername: process.env.REDISCACHEHOSTNAME
+        }
+    });
+
+    client.on("error", function (err) {
+        context.log("Error " + err);
+    });
+
+    scb().then(page => {
+        if (page.data.beer.count === 12) { //currently we expect 12 beers on tap
+            client.set(process.env.BEERVANACACHEOBJECTNAME, JSON.stringify(page.data.beer), redis.print);
+        }
+    });
+
+    context.log('savingDataToRedis finished');
+};
